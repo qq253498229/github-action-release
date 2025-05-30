043d13f2eb9360651a13251ed0ec3c3ef644e5c7
大胃王 <253498229@qq.com>
2025-05-30 17:56:52 +0800

    test4

--- src/main.ts
+++ src/main.ts
@@ -98,7 +98,9 @@ export async function run(): Promise<void> {
           data,
           name,
           headers: {
-            'X-GitHub-Api-Version': '2022-11-28'
+            'X-GitHub-Api-Version': '2022-11-28',
+            Accept: 'application/vnd.github+json',
+            'Content-Type': 'application/octet-stream'
           }
         }
       )
